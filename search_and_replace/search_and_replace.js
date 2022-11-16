95a5ac444462d276035e75a3cd9a9a61981c526a
guv-slime <emoryhare@gmail.com>
2022-11-16 10:12:58 -0500

    Completed the Search and Replace challenge on FCC. I feel like there's a more concise way to do this one. I created an isUpperFunction that returns true or false if the first letter of the before word is capitalized. Then I split the string into an array based on spaces. Then I find the index of before in the array. I made a variable called caseWord that uses a substring to grab all characters except for the first one. Then the program checks if the before word is capitalized and if it is, I capitalize the first character of after and concatenate caseWord onto the end and returns arr.join on spaces. If it's not capitalized, the else statement activates and sets the first character in after to be lowercase and concatenates the caseWord onto the end and returns arr.join on spaces.

---
+++ search_and_replace/search_and_replace.js
@@ -0,0 +1,25 @@
+function isUpper(str){
+  console.log(str)
+  if(str[0] === str.toUpperCase()){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function myReplace(str, before, after) {
+  let arr = str.split(' ');
+  let location = arr.indexOf(before);
+  let caseWord = after.substring(1);
+
+  console.log(arr[location])
+  if(isUpper(arr[location][0]) === true){
+    arr[location] = after[0].toUpperCase() + caseWord;
+    return arr.join(' ');
+  } else {
+    arr[location] = after[0].toLowerCase() + caseWord;
+    return arr.join(' ');
+  }
+}
+
+myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "all");
