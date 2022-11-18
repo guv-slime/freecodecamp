ced7790ff8f991226d4387a2fde51fe378d4d9ca
guv-slime <emoryhare@gmail.com>
2022-11-18 14:19:31 -0500

    Used indexOf, split, and includes to solve this one. There's definitely an easier way to solve this, so I will return to it later to come up with a better solution.

--- missing_letters/missing_letters.js
+++ missing_letters/missing_letters.js
@@ -1,5 +1,17 @@
-function fearNotLetter(str) {
-  return str;
-}
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+function fearNotLetter(str) {
+  let first = alphabet.indexOf(str[0]);
+  let myLetters = str.split('');
+
+  if(myLetters.length === alphabet.length){
+    return undefined;
+  } else {
+    for(let i = first; i < first + str.length; i++) {
+      if(!myLetters.includes(alphabet[i])){
+        return alphabet[i];
+      }
+    }
+  }
+}
 fearNotLetter("abce");
